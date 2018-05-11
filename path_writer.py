d39eff2bc0f71149a70dafc1719134459779ed9a
Chillylin the Beaver <32764773+chillylin@users.noreply.github.com>
2018-05-11 23:33:45 +1200

    Update path_writer.py

--- path_writer.py
+++ path_writer.py
@@ -8,9 +8,9 @@ for root, dirs, files in os.walk(os.path.abspath("...")):
     for file in files:
         path.append(os.path.join(root, file))
 
-path = np.array(path)
-print(path)
+
+
 
 with open(".../.csv", 'w') as myfile:
-    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    wr.writerow(path)
+    for singlefile in path:
+        myfile.write(singlefile+'\n')
