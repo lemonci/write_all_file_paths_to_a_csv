d9a062a4599ab85c01b0e9090d9ecdb3d695b6a8
lemonci <hongyanchangfei@gmail.com>
2018-05-14 13:04:42 +0800

    Update path_writer.py

--- path_writer.py
+++ path_writer.py
@@ -2,6 +2,21 @@ import os
 import csv
 import numpy as np
 
+
+def on_click():
+    path = []
+
+    for root, dirs, files in os.walk(os.path.abspath(home)):
+        for file in files:
+            path.append(os.path.join(root, file))
+
+    path = np.array(path)
+    print(path)
+
+    with open(csvloc, 'w') as myfile:
+        for singlefile in path:
+            myfile.write(singlefile+'\n')
+
 #GUI Start
 
 from tkinter import *
@@ -32,15 +47,3 @@ root.mainloop()
 
 #GUI End
 
-path = []
-
-for root, dirs, files in os.walk(os.path.abspath(home)):
-    for file in files:
-        path.append(os.path.join(root, file))
-
-path = np.array(path)
-print(path)
-
-with open(csvloc, 'w') as myfile:
-    for singlefile in path:
-        myfile.write(singlefile+'\n')
