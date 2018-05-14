45fbf16da30901b563bec551c98ec557adeebcec
lemonci <hongyanchangfei@gmail.com>
2018-05-14 14:08:56 +0800

    Update path_writer.py

--- path_writer.py
+++ path_writer.py
@@ -3,6 +3,7 @@ import csv
 
 # write file path into a csv file
 def on_click():
+    from subprocess import Popen
     path = []
     
     homedir = str(home.get()) # get content from home and convert it to string
@@ -17,16 +18,23 @@ def on_click():
     with open(csvlocdir, 'w') as myfile:
         for singlefile in path:
             myfile.write(singlefile+'\n')
-# path writer end
             
+    p = Popen('filename.csv', shell=True)
+    subprocess.Popen(csvlocdir,'r')
+# path writer end
+
   
 #GUI Start
 
 from tkinter import *
 
-
 root = Tk()
-root.title()
+root.title("Path Writer")
+root.geometry('400x200')
+
+#clost window
+def close_window ():
+    root.destroy()
 
 #GUI Line 1 : Prompt for home folder
 l1 = Label(root, text="Home folder path:")
@@ -34,8 +42,8 @@ l1.pack()
 
 #GUI Line 2 : Receiving home folder as a string
 home_folder = StringVar()
-home = Entry(root, textvariable = home_folder)
-home_folder.set(" ")
+home = Entry(root, textvariable = home_folder, width=40)
+home_folder.set("")
 home.pack()
 
 #GUI Line 3 Prompt for csv location
@@ -44,12 +52,23 @@ l2.pack()
 
 #GUI Line 4 Receiving csv location as a string
 csv_loc = StringVar()
-csvloc = Entry(root, textvariable = csv_loc)
-csv_loc.set(" ")
+csvloc = Entry(root, textvariable = csv_loc, width=40)
+csv_loc.set("")
 csvloc.pack()
 
-#GUI Line 5 "OK Button", click to execute mainloop
+#GUI Line 5 Reminder
+l3 = Label(root, text="NOTE: PLEASE CHANGE ALL \ INTO / BEFORE CLICK OK!")
+l3.pack()
+
+
+#GUI Line 6 "OK Button", click to execute mainloop
 Button(root, text="OK", command = on_click).pack()
+
+#GUI Line 7 "Exit Button", click to exit mainloop
+frame = Frame(root)
+frame.pack()
+Button(root, text="Exit", command = close_window).pack()
+
 
 root.mainloop()
 
