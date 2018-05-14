bea98e37f84d6bcd6b2dd40dc721af4ed1d50e62
lemonci <hongyanchangfei@gmail.com>
2018-05-14 12:48:09 +0800

    Update path_writer.py

--- path_writer.py
+++ path_writer.py
@@ -2,15 +2,43 @@ import os
 import csv
 import numpy as np
 
+#GUI Start
+
+from tkinter import *
+root.title()
+
+l1 = Label(root, text="home folder path：")
+l1.pack()
+home_folder = StringVar()
+home = Entry(root, textvariable = home_folder)
+home_folder.set(" ")
+home.pack()
+
+
+l2 = Label(root, text="csv location")
+l2.pack()
+csv_loc = StringVar()
+csvloc = Entry(root, textvariable = csv_loc)
+csv_loc.set(" ")
+csvloc.pack()
+
+Button(root, text="OK", command = on_click).pack()
+
 path = []
 
-for root, dirs, files in os.walk(os.path.abspath("...")):
+root.mainloop()
+
+#GUI End
+
+path = []
+
+for root, dirs, files in os.walk(os.path.abspath(home)):
     for file in files:
         path.append(os.path.join(root, file))
 
 path = np.array(path)
 print(path)
 
-with open(".../.csv", 'w') as myfile:
+with open(csvloc, 'w') as myfile:
     for singlefile in path:
         myfile.write(singlefile+'\n')
