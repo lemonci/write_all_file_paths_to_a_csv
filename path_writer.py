958a8a71cfd584fbc2c861e2cfc5bf59b0b805e7
lemonci <hongyanchangfei@gmail.com>
2018-05-14 12:55:09 +0800

    Update path_writer.py

--- path_writer.py
+++ path_writer.py
@@ -7,21 +7,23 @@ import numpy as np
 from tkinter import *
 root.title()
 
-l1 = Label(root, text="home folder path：")
+#GUI Line 1 "home folder path: [user inpt]"
+l1 = Label(root, text="home folder path:")
 l1.pack()
 home_folder = StringVar()
 home = Entry(root, textvariable = home_folder)
 home_folder.set(" ")
 home.pack()
 
-
-l2 = Label(root, text="csv location")
+#GUI Line 2 "csv location: [user inpt]"
+l2 = Label(root, text="csv location:")
 l2.pack()
 csv_loc = StringVar()
 csvloc = Entry(root, textvariable = csv_loc)
 csv_loc.set(" ")
 csvloc.pack()
 
+#GUI Line 3 "OK Button", click to execute mainloop
 Button(root, text="OK", command = on_click).pack()
 
 path = []
